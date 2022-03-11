374b7199e2e05a64b93fcaa49e69bac8f5cbd9ba
KangLee <1840495919@qq.com>
2022-03-11 12:15:56 +0800

    taco cloud 0.0.1

---
+++ taco-cloud/src/test/java/com/example/demo/test/HomeControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.demo.test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.controller.HomeController;
+
+@SpringBootTest
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testHomePage() throws Exception {
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("home"))
+				.andExpect(content().string(containsString("Welcome to...")));
+		
+		
+	}
+	
+
+}
